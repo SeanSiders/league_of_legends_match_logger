12f2b8ab63da52704545196e73ba6eb52671e39d
Sean Siders <sean.siders@icloud.com>
2022-10-23 16:23:00 -0700

    Node.js web app initialized.
    index.html contains an HTML table for each table that will be in the db.

---
+++ app/index.js
@@ -0,0 +1,21 @@
+'use strict';
+import express from 'express';
+
+const PORT = 3000;
+
+const app = express();
+
+//Automatically decode the URL in POST methods
+app.use(express.urlencoded({
+    extended: true
+}));
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile('/index.html');
+})
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}...`);
+});
