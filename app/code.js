60844fc6acfa1f174826b8751d4e5e3dd8ebbd6d
jeremysteele <jeremysteelewriter@gmail.com>
2022-10-25 23:41:12 -0700

    2.0 solid?!

--- app/code.js
+++ app/code.js
@@ -1,10 +1,13 @@
 
 
-const TeemoJS = require('teemojs');
-let api = TeemoJS('RGAPI-KEY-HERE');
+//const TeemoJS = require('teemojs');
+//let api = TeemoJS('RGAPI-KEY-HERE');
 
 
 const url = "https://developer.riotgames.com//riot/account/v1/accounts/by-puuid/{puuid}"
-async function getRequest(summoner) {
+//async function getRequest(summoner) {}};
 
-}
+const button = document.getElementById('myButton');
+button.addEventListener('click', function(e) {
+  console.log('button was clicked');
+});
