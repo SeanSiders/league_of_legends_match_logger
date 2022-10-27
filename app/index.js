0d9d9380a23a2c8ca638730f9d3c998fb45436fa
jeremysteele <jeremysteelewriter@gmail.com>
2022-10-26 18:47:06 -0700

    2.5

--- app/index.js
+++ app/index.js
@@ -1,11 +1,13 @@
 'use strict';
 const express = require('express');
+var bodyParser = require("body-parser");
+
 var PORT = 3000;
 var app = express();
 
 //Automatically decode the URL in POST methods
 app.use(express.urlencoded({
-    extended: true
+    extended: false
 }));
 
 app.use(express.static('public'));
@@ -22,10 +24,19 @@ app.get('/champions', (req, res) => {
     res.sendFile('/champions.html');
 });
 
+app.post('/champion', (req, res) => {
+    console.log(req.body)
+    res.sendFile('/champions.html', )
+})
+
+app.get('/summoner', (req, res) => {
+    res.sendFile('/summoner.html');
+});
 app.get('/match', (req, res) => {
     res.sendFile('/match.html');
 });
 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}...`);
+    console.log('hello');
 });
